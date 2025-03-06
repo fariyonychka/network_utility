18c8bcef5632d98dd253e28aa7f967b6fc1b04ec
Yustyna <yustyna1409a@gmail.com>
2025-03-06 14:49:54 +0000

    Added main function with CLI argument parsing

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "traceroute.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <hostname> <maxHops>\n";
+        return 1;
+    }
+    std::string hostname = argv[1];
+    int maxHops = std::stoi(argv[2]);
+    traceRoute(hostname, maxHops);
+    return 0;
+}
